127bba324c553e014be1575e5f96be967e5aa138
MSKU786 <manishsingh332000@gmail.com>
2025-05-22 11:27:20 +0530

    updated latest version of clerk

--- Client/marketplace/middleware.js
+++ Client/marketplace/middleware.js
@@ -1,32 +1,24 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// const isProtectedRoute = createRouteMatcher([
-//   '/admin(.*)?',
-//   '/saved-cars(.*)?',
-//   '/reservations(.*)?',
-// ]);
+const isProtectedRoute = createRouteMatcher([
+  '/admin(.*)?',
+  '/saved-cars(.*)?',
+  '/reservations(.*)?',
+]);
 
-// export default clerkMiddleware(async (auth, req) => {
-//   const { userId, redirectToSignIn } = await auth();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
 
-//   if (!userId && isProtectedRoute(req)) {
-//     return redirectToSignIn();
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and static files
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API and trpc
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-
-import { authMiddleware } from '@clerk/nextjs/server';
-
-export default authMiddleware();
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api)(.*)'],
+  matcher: [
+    // Skip Next.js internals and static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API and trpc
+    '/(api|trpc)(.*)',
+  ],
 };
